42c47e40761335eca04a0052f70589ae3943e6d9
ben-thebauer <benbauer314@gmail.com>
2016-01-16 11:59:24 -0800

    CANJaguars

---
+++ src/org/usfirst/frc/team670/robot/commands/Move.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team670.robot.commands;
+
+import org.usfirst.frc.team670.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class Move extends Command {
+	
+	private double startTime;
+	private double period;
+	private double amount;
+	private double amountSide;
+	
+    public Move(double p, double a, double as) {
+        requires(Robot.driveTrain);
+        period = p;
+        amount = a;
+        amountSide = as;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	startTime = System.currentTimeMillis();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() 
+    {
+    	Robot.driveTrain.drive(amount, amount, amountSide);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return (System.currentTimeMillis() - startTime) > period * 1000;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() 
+    {
+    	Robot.driveTrain.drive(0, 0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() 
+    {
+    	end();
+    }
+}
